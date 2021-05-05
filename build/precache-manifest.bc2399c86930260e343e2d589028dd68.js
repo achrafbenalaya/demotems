4823db1aa2d4eb807b339047930dd22cc712938a
Achraf ben alaya <achrafbenalaya@users.noreply.github.com>
2021-05-05 12:56:10 +0100

    build

---
+++ build/precache-manifest.bc2399c86930260e343e2d589028dd68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c663dffb5be9c211d8d3cd5e6705df3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e866f4bc7fddf8e2338c",
+    "url": "/static/css/main.80168001.chunk.css"
+  },
+  {
+    "revision": "46c3f05added4616ce35",
+    "url": "/static/js/2.a7d1d6ac.chunk.js"
+  },
+  {
+    "revision": "4c98c6b556b8cc39dae85626e0284631",
+    "url": "/static/js/2.a7d1d6ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e866f4bc7fddf8e2338c",
+    "url": "/static/js/main.2261c2bb.chunk.js"
+  },
+  {
+    "revision": "9bfd6d225e4168839554",
+    "url": "/static/js/runtime-main.22145939.js"
+  }
+]);
